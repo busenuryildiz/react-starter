64c188512cca1f5617646250ba8c497c80a750f7
busenuryildiz <busenuryildiz5337@gmail.com>
2023-12-29 19:34:48 +0300

    added removeFromCart and clearCart

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {Button} from "semantic-ui-react";
-import {useDispatch} from "react-redux";
-import {addToCart} from "../../store/actions/cartActions";
+import { Button } from "semantic-ui-react";
+import { useDispatch } from "react-redux";
+import { addToCart, clearCart, removeFromCart } from "../../store/actions/cartActions";
 function Posts() {
 	const dispatch = useDispatch();
 
@@ -10,10 +10,24 @@ function Posts() {
 			Posts
 			<Button
 				onClick={() => {
-					dispatch(addToCart({id: 1, name: "Laptop", price: 10}));
+					dispatch(addToCart({ id: 1, name: "Laptop", price: 10 }));
 				}}
 			>
 				Sepete Ekle
+			</Button>
+			<Button
+				onClick={() => {
+					dispatch(removeFromCart({ id: 1 }));
+				}}
+			>
+				Sepetten Çıkar
+			</Button>
+			<Button
+				onClick={() => {
+					dispatch(clearCart());
+				}}
+			>
+				Sepeti Temizle
 			</Button>
 		</div>
 	);
