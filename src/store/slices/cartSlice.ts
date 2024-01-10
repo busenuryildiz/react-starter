d5c0a69de3837a565c679df8d4a85159f3efedb5
busenuryildiz <busenuryildiz5337@gmail.com>
2024-01-10 18:37:12 +0300

    developed

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -2,22 +2,24 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
 	name: "cart",
-	initialState: {cartItems: [] as any[]},
+	initialState: {cartItems: JSON.parse(localStorage.getItem("cart")!) || []},
 	reducers: {
 		addToCart: (state, action) => {
-			// ynı üründen 2. kez eklendiğinde listeye eklenmesi yerine adedinin artırılması..
+			// aynı üründen 2. kez eklendiğinde listeye eklenmesi yerine adedinin artırılması..
 			state.cartItems.push(action.payload);
+			localStorage.setItem("cart", JSON.stringify(state.cartItems));
 		},
 		removeFromCart: (state, action) => {
 			state.cartItems = state.cartItems.filter(
 				(item: any) => item.id !== action.payload.id,
 			);
+			localStorage.setItem("cart", JSON.stringify(state.cartItems));
 		},
 		clearCart: state => {
 			state.cartItems = [];
+			localStorage.setItem("cart", JSON.stringify(state.cartItems));
 		},
 	},
 });
-
 export const cartReducer = cartSlice.reducer;
 export const cartActions = cartSlice.actions;
